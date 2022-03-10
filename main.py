d0dc0cd6921a471c01bb43d8319a36892ea421c1
Jin Xian Li <jli452@binghamton.edu>
2022-03-10 03:04:40 -0500

    not using opencv too slow

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-import numpy as np
+import logging
+import os
+import sys
 import pyautogui
-import cv2
 import time
 from time import gmtime, strftime
-import random
 
-flag = "pulled"
-found = False
-template = cv2.imread("template.png")
-method = cv2.TM_SQDIFF
-counter = 0
-threshold = .8
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
-def is_template_in_image(img, templ):
-    res = cv2.matchTemplate(img, templ, method)
-    min_val = cv2.minMaxLoc(res)[0]
-    return min_val <= threshold
+    return os.path.join(base_path, relative_path)
 
-print(strftime("%H:%M:%S", gmtime()), "starting a bot")
-time.sleep(3)
+def is_template_in_image(img):
+    return pyautogui.locateOnScreen(img, confidence=0.8) is not None
 
-while(1):
-    if flag == "pulled":
-        print(strftime("%H:%M:%S", gmtime()), "throwing a fishing rod,", counter)
-        pyautogui.press('e')
-        flag = "thrown"
+def run_bot():
+    flag = "pulled"
+    found = False
+    template = resource_path('test1.png')
+    counter = 0
     
-    if flag == "thrown":
-        screenshot = pyautogui.screenshot()
-        screenshot.save(r'C:\Users\liron\Downloads\fishbot\screenshot.png')
-        screenshot = cv2.imread("screenshot.png")
-        res = cv2.matchTemplate(screenshot, template, method)
-        if (is_template_in_image(screenshot, template)):
-            print(strftime("%H:%M:%S", gmtime()), "Time to fish!")
+    print(strftime("%H:%M:%S", gmtime()), "starting a bot")
+    time.sleep(3)
+    while(1):
+        if flag == "pulled":
+            print(strftime("%H:%M:%S", gmtime()), "throwing a fishing rod,", counter)
             pyautogui.press('e')
-            flag = "pulled"
-            counter+=1
-            found = False
-            time.sleep(200)
+            flag = "thrown"
+        
+        elif flag == "thrown":
+            if (is_template_in_image(template)):
+                print(strftime("%H:%M:%S", gmtime()), "Time to fish!")
+                pyautogui.press('e')
+                flag = "pulled"
+                counter+=1
+                found = False
+                time.sleep(6)
+def main():
+    run_bot()
+
+if __name__ == "__main__":
+    main()
