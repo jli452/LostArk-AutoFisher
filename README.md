8fa07578c84df041b7ddf845790bea00eca1d48a
jli452 <jli452@binghamton.edu>
2022-03-14 03:11:23 -0400

    added auto chaos dungeon raids for gunlancer chaos build

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # LostArk-AutoFisher
 AutoFisher for a popular MMO Lost Ark
+
+How to run:
+First install pyautogui using the command in terminal:
+- pip install pyautogui (on Windows)
+- pip3 install pyautogui (on macOS and Linux)
+
+Then run file by typing in:
+-main.py
+
+Proceed with program that you want to use
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import logging
 import os
 import sys
 import pyautogui
 import time
-from time import gmtime, strftime
 
 def resource_path(relative_path):
     try:
@@ -16,30 +14,90 @@ def resource_path(relative_path):
 def is_template_in_image(img):
     return pyautogui.locateOnScreen(img, confidence=0.65) is not None
 
-def run_bot():
-    flag = "pulled"
-    found = False
+#needed because py autogui click doesnt register for first one only moves mouse
+def right_click(x, y):
+    pyautogui.click(x, y, button='right')
+    pyautogui.click(x, y, button='right')
+
+def left_click(x, y):
+    pyautogui.click(x, y)
+    pyautogui.click(x, y)
+
+def hold_key(key, hold_time):
+    pyautogui.keyDown(key)
+    time.sleep(hold_time)
+    pyautogui.keyUp(key);
+
+def auto_fish():
+    flag = "thrown"
     template = resource_path('punika.png')
     counter = 0
-    
-    print(strftime("%H:%M:%S", gmtime()), "starting a bot")
-    time.sleep(3)
-    #x, y = pyautogui.locateCenterOnScreen('horse.png', confidence=0.8)
-    #pyautogui.click(x, y)
+
+    print("Throw the fishing rod in and afk to get started")
     while(1):
         if flag == "pulled":
-            print(strftime("%H:%M:%S", gmtime()), "throwing a fishing rod,", counter)
+            print("throwing a fishing rod. Times done:", counter)
             pyautogui.press('e')
             flag = "thrown"
         
         elif flag == "thrown":
             if (is_template_in_image(template)):
-                print(strftime("%H:%M:%S", gmtime()), "Time to fish!")
+                print("Time to fish!")
                 pyautogui.press('e')
                 flag = "pulled"
                 counter+=1
-                found = False
                 time.sleep(6)
+
+def chaos_gunlancer():
+    x, y = pyautogui.locateCenterOnScreen('statue.png', confidence=0.4)
+        right_click(x, y)
+        time.sleep(1)
+        x, y = pyautogui.locateCenterOnScreen('enter.png', confidence=0.8)
+        left_click(x, y)
+        time.sleep(1)
+        x, y = pyautogui.locateCenterOnScreen('accept.png', confidence=0.8)
+        left_click(x, y)
+        time.sleep(7)
+        right_click(x, y)
+        time.sleep(8)
+        pyautogui.press('e')
+        time.sleep(1)
+        pyautogui.press('f')
+        time.sleep(8)
+        hold_key('s', 3.5)
+        time.sleep(8)
+        pyautogui.press('f')
+        time.sleep(6.5)
+        pyautogui.press('e')
+        time.sleep(1.5)
+        pyautogui.press('z')
+        time.sleep(4)
+        hold_key('s', 3.5)
+        time.sleep(6)
+        pyautogui.press('e')
+        time.sleep(1)
+        pyautogui.press('f')
+        time.sleep(6)
+        hold_key('s', 3.5)
+        x, y = pyautogui.locateCenterOnScreen('leave.png', confidence=0.8)
+        left_click(x, y)
+        pyautogui.press('enter')
+        time.sleep(15)
+
+def auto_chaos():
+    print("Starting in 5 seconds. Tab back in the game and be near a chaos dungeon statue (punika if possible).")
+    time.sleep(5)
+    while(1):
+        chaos_gunlancer()
+
+
+def run_bot():    
+    text = input("What would you like to do in Lost Ark? Current commands: [fish] [chaos (Gunlance Only)]\n")
+    if text == "fish":
+        auto_fish()
+    elif text == "chaos":
+        auto_chaos()
+
 def main():
     run_bot()
 
